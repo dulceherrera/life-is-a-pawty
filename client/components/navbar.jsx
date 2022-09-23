b59e09397f9a255ec5978912da91015b9bd1684f
Dulce Herrera <dulceherrera11@gmail.com>
2022-09-23 18:21:24 +0000

    Add a drawer component to the navbar feature-4

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -2,15 +2,31 @@ import React from 'react';
 
 export default function Navbar(props) {
   return (
-    <header>
-      <nav className='navbar navbar-expand-lg p-0 navbar-light purple-background py-0 justify-content-center'>
-        <div className='p-2 d-flex justify-content-center purple-background py-0 align-items-center h-6' >
-          <a className='fs-0 text-white font-patrick title-nav d-flex justify-content-center' href='#'>Life&apos;s a Pawty</a>
-          <a href='#' className='d-flex justify-content-center width-10'>
-            <img className='icon-home' src='/images/catdog.svg'/>
-          </a>
+    <header className='d-flex purple-background'>
+      <i className="fa-solid fa-bars text-white" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-controls='collapseExample'></i>
+      <div className='collapse collapse-start' tabIndex='-1' id='collapseExample' aria-labelledby='collapseExampleLabel'>
+        <div className='collapse-header'>
+          <h1 id='collapseExampleLabel' className='collapse-title'>Life&apos;s a Pawty</h1>
         </div>
-      </nav>
+        <div className='collapse-body'>
+          <div className='justify-content-center nav-links'>
+            <a className='text-decoration-none text-black d-flex' href='#'>
+              <img src='/images/home-icon.png' alt='home-icon' className='home-icon'></img>
+              <h3>Home</h3>
+            </a>
+            <a className='justify-content-center d-flex text-black' href='#'>
+              <img src='/images/your-pets.png/' alt='your-pets-icon'></img>
+              <h3>Your Pets</h3>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='d-flex justify-content-center purple-background py-0'>
+        <a className='fs-0 text-white font-patrick title-nav d-flex justify-content-center' href='#'>Life&apos;s a Pawty</a>
+        <a href='#' className='d-flex justify-content-center width-10'>
+        <img className='icon-home' src='/images/catdog.svg'/>
+        </a>
+      </div>
     </header>
   );
 }
