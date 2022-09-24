26c31f14104746f2b8f295d77616d12b6e732975
Dulce Herrera <dulceherrera11@gmail.com>
2022-09-24 20:13:53 +0000

    Completed feature-4-view-matches

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -3,25 +3,26 @@ import React from 'react';
 export default function Navbar(props) {
   return (
     <header className='d-flex purple-background'>
-      <i className="fa-solid fa-bars text-white" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-controls='collapseExample'></i>
-      <div className='collapse collapse-start' tabIndex='-1' id='collapseExample' aria-labelledby='collapseExampleLabel'>
-        <div className='collapse-header'>
-          <h1 id='collapseExampleLabel' className='collapse-title'>Life&apos;s a Pawty</h1>
+    <i className="fa-solid fa-bars text-white" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"></i>
+      <div className="offcanvas offcanvas-start" tabIndex="-1" id='offcanvasExample' aria-labelledby="offcanvasExampleLabel">
+        <div className="offcanvas-header mb-4">
+          <h1 id="offcanvasExampleLabel" className="offcanvas-title font-patrick">Life&apos;s a Pawty</h1>
+          <button type="button" className="btn-close text-reset" aria-label="Close" data-bs-dismiss="offcanvas"></button>
         </div>
-        <div className='collapse-body'>
-          <div className='justify-content-center nav-links'>
-            <a className='text-decoration-none text-black d-flex' href='#'>
+        <div className="offcanvas-body">
+          <div className="nav-links">
+            <a className='text-decoration-none text-black d-flex mb-4rem align-items-center drawer-text' href='#'>
               <img src='/images/home-icon.png' alt='home-icon' className='home-icon'></img>
-              <h3>Home</h3>
+              <h3 className='font-quicksand'>Home</h3>
             </a>
-            <a className='justify-content-center d-flex text-black' href='#'>
-              <img src='/images/your-pets.png/' alt='your-pets-icon'></img>
-              <h3>Your Pets</h3>
+            <a className='text-black d-flex align-items-center text-decoration-none drawer-text' href='#saved-pets'>
+              <img src='/images/your-pets.png' alt='your-pets-icon' className="your-pets-icon"></img>
+              <h3 className='font-quicksand'>Your Pets</h3>
             </a>
           </div>
         </div>
       </div>
-      <div className='d-flex justify-content-center purple-background py-0'>
+      <div className='d-flex justify-content-center purple-background py-0 container-fluid'>
         <a className='fs-0 text-white font-patrick title-nav d-flex justify-content-center' href='#'>Life&apos;s a Pawty</a>
         <a href='#' className='d-flex justify-content-center width-10'>
         <img className='icon-home' src='/images/catdog.svg'/>
---
+++ client/pages/saved.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export default class SavedPets extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animals: []
+    };
+    this.handleSavedPets = this.handleSavedPets.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleSavedPets();
+  }
+
+  handleSavedPets() {
+    fetch('/api/matches')
+      .then(res => res.json())
+      .then(animals => this.setState({ animals }))
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    return (
+    <>
+    <h1 className='font-quicksand d-flex justify-content-center mt-5 your-pets-title'>YOUR PETS</h1>
+    <div className='row row-cols-1 row-cols-md-3 g-4 m-2'>
+      {this.state.animals.map(animal => {
+        return (
+          <div key={animal.petId} className='col'>
+            <div className='h-50 card bg-transparent'>
+              <img src={animal.photos} className='card-img-top' alt='saved-pet'></img>
+              <div className='card-body font-quicksand text-align-center bg-pink border-purple'>
+              <h2 className='card-title'>{animal.name}</h2>
+              <p className='card-text text-saved'><span className="fw-bolder">Location: </span> {animal.location}</p>
+              <p className='card-text text-saved'><span className="fw-bolder">Age: </span> {animal.age}</p>
+              <p className='card-text text-saved'><span className="fw-bolder">Breed:</span> {animal.breed}</p>
+              <p className='card-text text-saved'><span className="fw-bolder">Gender:</span> {animal.gender}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+    </>
+    );
+  }
+}
